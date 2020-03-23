7f8a6093c2685d5eab1b4b8b2a44d494ee975c58
eppisapia <eduardo.pisapia@synergy-gb.com>
2020-03-23 12:25:36 -0400

    Created Balance Screen component

---
+++ src/pages/Balance.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { IonPage, IonContent } from '@ionic/react';
+
+import './Balance.css';
+
+const Balance: React.FC = () => {
+    return (
+        <IonPage>
+        </IonPage>
+    );
+};
+
+export default Balance;
+
