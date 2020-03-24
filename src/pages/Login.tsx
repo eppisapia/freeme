b0e94272885db62863ec05681ebd92e3652814ff
eppisapia <eduardo.pisapia@synergy-gb.com>
2020-03-24 14:51:03 -0400

    Added firebase config files. Fixed Login scroll

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonPage, IonImg } from '@ionic/react';
+import { IonPage, IonImg, IonContent } from '@ionic/react';
 
 import LoginForm from '../components/LoginForm';
 import { RouteComponentProps } from 'react-router';
@@ -9,8 +9,12 @@ import './Login.css';
 const Login: React.FC<RouteComponentProps> = (props) => {
     return (
         <IonPage className="signinPage">
-            <IonImg src='assets/sign-in/logo-freeme.svg' className="logo" />
-            <LoginForm nav={props} />
+            <IonContent >
+                <div className="loginContainer">
+                    <IonImg src='assets/sign-in/logo-freeme.svg' className="logo" />
+                    <LoginForm nav={props} />
+                </div>
+            </IonContent>
         </IonPage>
     );
 };
